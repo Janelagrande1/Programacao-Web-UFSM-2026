229ca6c04af7a137f6369ca0e2ba84aa83c018e9
B.Lima <janelagrande0@gmail.com>
2026-04-13 16:51:36 -0300

    Criado Questão 2 e Adicionado Enunciado
    Adicionado Enunciado da Questão 2

---
+++ 2Aula09,03,26/Q2-Lista1.php
@@ -0,0 +1,59 @@
+<?php
+// Questão 2 da Lista 1
+/* Analise os algoritmos abaixo, reescreva-os na linguagem PHP e diga o que será impresso na 
+tela ao serem executados: */
+/* 1a
+A <-- 10
+B <-- 20
+Mostra B
+B <-- 5
+Mostra A, B */
+
+/* 2b
+A <-- 30
+B <-- 20
+C <-- A + B
+Mostra C
+B <-- 10
+Mostra B, C
+C <-- A + B
+Mostra A, B, C */
+
+/* 3c
+A <-- 10
+B <-- 20
+C <-- A
+B <-- C
+A <-- B
+Mostra A, B, C */
+
+/* 4d
+A <-- 10
+B <-- A + 1
+A <-- B + 1
+B <-- A + 1
+Mostra A
+A <-- B + 1
+Mostra A, B */
+
+/* 5e
+A <-- 10
+B <-- 5
+C <-- A + B
+B <-- 20
+A <-- 10
+Mostra A, B, C */
+
+/* 6f
+X <-- 1
+Y <-- 2
+Z <-- Y - X
+Mostra Z
+X <-- 5
+Y <-- X + Z
+Mostra X, Y, Z */
+
+
+
+//b/ Bruno SL - 13/04/2026
+?>
