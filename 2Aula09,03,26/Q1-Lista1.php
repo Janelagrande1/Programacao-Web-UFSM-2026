321a8a4f95f971086e012f51ae83ce4ac48a6f8a
B.Lima <janelagrande0@gmail.com>
2026-04-13 16:28:09 -0300

    Algum Problema no GitDesktop
    Commit para manter a regularidade

--- 2Aula09,03,26/Q1-Lista1.php
+++ 2Aula09,03,26/Q1-Lista1.php
@@ -1,3 +1,39 @@
 <?php
 print "Ola Mundo";
 ?>
+// Questão 1 da Lista 1
+/* Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma 
+variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos 
+fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores 
+que ficaram armazenados nas variáveis. */
+
+// Variaveis, somente duas
+
+$a = 10;
+$b = 20;
+
+// Mostra Original
+
+print "Valor original de A: $a <br>";
+print "Valor original de B: $b <br>";
+
+// Motor responsável pela troca
+
+$a = $a + $b;
+
+// Antes A 10 e B 20
+// Agora A 30 e B 20
+// Depois A  e B 
+
+
+
+// Mostra Alteração
+
+print "Novo Valor de A: $a <br>";
+print "Novo Valor de B: $b <br>";
+
+
+
+
+//b/ Bruno SL - 03/26/2024
+?>
